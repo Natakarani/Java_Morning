bd0452fd541f747e46038da9153e768167addd41
Natakarani <139905162+Natakarani@users.noreply.github.com>
2024-02-06 13:19:04 +0530

    Update Function_I.java

--- Java_Morning/com/studyopedia1/Function_I.java
+++ Java_Morning/com/studyopedia1/Function_I.java
@@ -7,13 +7,23 @@ public void m1() {
 	System.out.println(a);
 	System.out.println(b);
 }
+public static void  m3() {
+	System.out.println("m3");
+}
+public static void m4() {
+	m3();
+	
+}
 public static void m2() {
 	System.out.println(b);
 	System.out.println(b);
 	System.out.println(new Function_I().a);
 }
 	public static void main(String[] args) {
-		
+		Function_I f=new Function_I();
+		f.m1();
+		m2();
+		m3();
 
 	}
 
@@ -21,4 +31,3 @@ public static void m2() {
 
 
 
-	
