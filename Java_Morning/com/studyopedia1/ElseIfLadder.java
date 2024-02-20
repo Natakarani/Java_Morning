fcb4f1a13bf8942aca2a3eed423d46d538156c96
Natakarani <139905162+Natakarani@users.noreply.github.com>
2024-02-20 11:58:12 +0530

    Create ElseIfLadder.java

---
+++ Java_Morning/com/studyopedia1/ElseIfLadder.java
@@ -0,0 +1,46 @@
+package com.studyopedia1;
+//5 different subjects check the average of it
+/*
+public class ElseIfLadder {
+	public static void getGrades(int marks) {
+		if(marks>=80) {
+			System.out.println("destinction");
+		}
+		else if(marks>=60 && marks<80) {
+				System.out.println("first class");
+			}
+		else if(marks>=40 && marks<60) {
+			System.out.println("second class");
+		}
+		else {
+			System.out.println("try again");
+		}
+		
+	}
+	public static void main(String[] args) {
+		getGrades(88);
+	
+
+	}
+
+}
+*/
+
+//for and while loop are pre-check loop
+//do while loop is post check loop
+
+
+
+
+public class ElseIfLadder {
+	public static void main(String[] args) {
+		 int i=1;
+		 do {
+			 System.out.println(i);//5
+		 }
+		 while(++i<=5);//while(i++<=5);
+	
+	}
+
+}
+
