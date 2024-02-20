d9c4cfe92870e3d537a10c17ceb315c0467ce5de
Natakarani <139905162+Natakarani@users.noreply.github.com>
2024-02-20 12:11:19 +0530

    Create Do_While.java

---
+++ Java_Morning/com/studyopedia1/Do_While.java
@@ -0,0 +1,30 @@
+package com.studyopedia1;
+/*
+public class Do_While {
+static int i=4;
+	public static void main(String[] args) {
+		do {
+			System.out.println(i);
+			i++;
+		}
+		while(i<8);
+		
+
+	}
+
+}
+*/
+
+
+
+public class Do_While {
+	public static void main(String[] args) {
+		 int i=1;
+		 do {
+			 System.out.println(i);//5
+		 }
+		 while(++i<=5);//while(i++<=5);
+	
+	}
+
+}
