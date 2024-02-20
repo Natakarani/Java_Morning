a415fc9fa0e70d51cb8abab6721a3093b110f0e7
Natakarani <139905162+Natakarani@users.noreply.github.com>
2024-02-20 11:52:30 +0530

    Create Average.java

---
+++ Java_Morning/com/studyopedia1/Average.java
@@ -0,0 +1,17 @@
+package com.studyopedia1;
+
+public class Average_I {
+
+	public static int AverageOfMarks() {
+		int telugu=98;
+		int hindi=80;
+		int english=90;
+		int average=(telugu+hindi+english)/3;
+		return average;
+	}
+		public static void main(String[] args) {
+			System.out.println(AverageOfMarks());
+			
+		}
+
+}
