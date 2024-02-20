f79e283cb5eef128c142e702af2706d3d030c185
Natakarani <139905162+Natakarani@users.noreply.github.com>
2024-02-20 12:10:23 +0530

    Create While_I.java

---
+++ Java_Morning/com/studyopedia1/While_I.java
@@ -0,0 +1,13 @@
+package com.studyopedia1;
+public class While {
+	public static void main(String[] args) {
+		 int i=4;
+		 
+		 while(i<=5) {
+			 System.out.println(i);
+			 i++;
+		 }
+	
+	}
+
+}
