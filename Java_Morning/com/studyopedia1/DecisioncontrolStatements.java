d5b22adddceaefeba878cb20f9a8182edfbfc0e8
Natakarani <139905162+Natakarani@users.noreply.github.com>
2024-02-20 11:59:39 +0530

    Create DecisioncontrolStatements.java

---
+++ Java_Morning/com/studyopedia1/DecisioncontrolStatements.java
@@ -0,0 +1,19 @@
+package com.studyopedia1;
+
+public class DecisioncontrolStatements {
+
+	public static void checkCondition() {
+		boolean result=true;
+		if(result) {
+			System.out.println("statement 1");
+		}
+		else {
+			System.out.println("else block");
+		}
+	}
+	public static void main(String[] args) {
+		checkCondition();
+
+	}
+
+}
